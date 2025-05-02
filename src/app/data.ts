a776e12982c34d11b664f2e10c07c9d0a1efa605
sallyibrahim117@gmail.com <sallyibrahim117@gmail.com>
2025-05-02 12:34:02 +0300

    fix links

--- src/app/data.ts
+++ src/app/data.ts
@@ -1,167 +1,318 @@
-
-export const allProducts=[   {title:"laptop & tablet" ,image:"/assets/imgs/pro1.jpg",
-  desc:"apple macBook pro with otuch bar mF841DSN/A 13",price: 1.259},
-  {title:"headphones" ,image:"/assets/imgs/pro2.jpg",
-  desc:"Ultra Wireless S50 Headphones with Bluetooth",price: 349},
-  {title:"Phones" ,image:"/assets/imgs/pro3.jpg",
-    desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 449},
-    {title:"Gadgets" ,image:"/assets/imgs/pro4.jpg",
-      desc:"Apple Watch Sport 42mm New",price: 349},
-
-      {title:"laptop & tablet" ,image:"/assets/imgs/pro1.jpg",
-        desc:"MacBook Pro Retina Touch Bar 2.9GHz Intel Core i7 16GB",price: 1.259},
-        {title:"Laptop & Tablet" ,image:"/assets/imgs/pro5.jpg",
-          desc:"MacBook Rose Gold 1.2GHz 256GB butterfly mechanism",price: 1.299},
-          {title:"Phones" ,image:"/assets/imgs/pro8.jpg",
-            desc:"Apple iPhone 7 4.7-Inch Silver 128GB Contact-Free",price: 250},
-            {title:"Phones" ,image:"/assets/imgs/pro3.jpg",
-              desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 351},
-              {title:"gadgets, headphones" ,image:"/assets/imgs/whitehead.jpg",
-                desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 225},
-                {title:"android" ,image:"/assets/imgs/and1.jpg",
-                  desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 449},
-];
-export const laptops=[{title:"laptop & tablet" ,image:"/assets/imgs/pro1.jpg",
-  desc:"apple macBook pro with otuch bar mF841DSN/A 13",price: 1.259},
-  {title:"laptop & tablet" ,image:"/assets/imgs/pro1.jpg",
-    desc:"MacBook Pro Retina Touch Bar 2.9GHz Intel Core i7 16GB",price: 1.259},
-    {title:"Laptop & Tablet" ,image:"/assets/imgs/pro5.jpg",
-      desc:"MacBook Rose Gold 1.2GHz 256GB butterfly mechanism",price: 1.299},
-      {title:"Laptop & Tablet" ,image:"/assets/imgs/and1.jpg",
-        desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 449},
-        {title:"Laptop & Tablet" ,image:"/assets/imgs/whitehead.jpg",
-          desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 225},
-]
-export const computers=[
-  {title:"Computers, Office" ,image:"/assets/imgs/mac.jpg",
-    desc:"MApple 21.5″ iMac® Intel Core i5 8GB 1TB Silver",price: 1259},
-    {title:"Cameras&Video, Computers" ,image:"/assets/imgs/mac.jpg",
-      desc:"Apple 21.5-inch iMac Retina 4K Display",price: 5559},
-  {title:"android" ,image:"/assets/imgs/and1.jpg",
-    desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 449},
-  {title:"gadgets, headphones" ,image:"/assets/imgs/whitehead.jpg",
-  desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 225},]
-
-  export const allHeadPhones=[ {title:"headphones" ,image:"/assets/imgs/pro2.jpg",
-    desc:"Ultra Wireless S50 Headphones with Bluetooth",price: 3149},
-    {title:"Gadgets" ,image:"/assets/imgs/pro4.jpg",
-      desc:"Apple Watch Sport 42mm New",price: 349},
-      {title:"gadgets, headphones" ,image:"/assets/imgs/whitehead.jpg",
-        desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 225},
-        {title:"gadgets, headphones" ,image:"/assets/imgs/cyanhead.jpg",
-          desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 2225},
-  ]
-export const phones=[{title:"iPhones" ,image:"/assets/imgs/and2.jpg",
-  desc:"Apple iPhone 6 4.7-Inch Black 128GB",price: 449},
-  {title:"iPhones" ,image:"/assets/imgs/and3.jpg",
-    desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 552},
-    {title:"android" ,image:"/assets/imgs/and4.jpg",
-      desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 1112},
-      {title:"iPhones" ,image:"/assets/imgs/and5.jpg",
-        desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 259},
-]
-
-
-export const released=[ {title:"Hot New Releases"},{title:"Top Selling Products",
-  }
-  ,{title:"Featured Products",
-    },
-
-]
-
-export const sellingPro=[{image:"/assets/imgs/pro5.jpg",
-  desc:"Apple VIP Membership",price: 259,oldPrice:299},{
-  image:"/assets/imgs/pro8.jpg",
-  desc:"	Apple iPad 9.7 128GB WiFi",price: 999}
-  ,{
-    image:"/assets/imgs/pro9.jpg",
-    desc:"	Apple iPad 9.7 128GB WiFi",price: 584},
-    {image:"/assets/imgs/pro2.jpg",
-      desc:"Apple VIP Membership",price: 259},{
-      image:"/assets/imgs/pro4.jpg",
-      desc:"	Apple iPad 9.7 128GB WiFi",price: 999}
-      ,{
-        image:"/assets/imgs/pro8.jpg",
-        desc:"	Apple iPad 9.7 128GB WiFi",price: 584},
-        {image:"/assets/imgs/and5.jpg",
-          desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 259},
-          {
-            image:"/assets/imgs/iron.jpg",
-            desc:"	Apple iPad 9.7 128GB WiFi",price: 999}
-            ,{image:"/assets/imgs/whitehead.jpg",
-              desc:"Apple iPhone 6s -16/32/64/ 128GB Grey",price: 225}
-  ]
-
-  // footer data
-
-  export const footerData=[{title:"about us"},{title:"our services"},{title:"contact us"},
-    {title:"stores"},{title:"order tracking"}]
-    export const customerService=[{title:"My account"},{title:"order tracking"},{title:"pricing plans"},{title:"terms & conditions"}
-
-      ,{title:"order tracking"},{title:"FQA"}
-    ]
-
-    export const quickLinks=[{title:"smartphones"},{title:"headphones"},{title:"Laptop & Tablet"},{title:"Gadgets"}];
-
-    export const page1=[{
-      label: "about us",link:"/about"}
-    ,{
-      label: "our team",link:"/our team"
-    }
-    ,{
-      label: "our services"
-    }
-    ,{
-      label: "pricing"
-    }
-    ,{
-      label: "contact"
-    }
-    ,{
-      label: "error 404"
-
-    }
-      ]
-// pages of navbar
-export const allPages=[{label:"home"},{label:"pages",
-  list:page1},{label:"shop"},{label:"blog"}
-  ,{label:"elements"}]
-
-  // pages of categories of header
-  export const page2=[
+export const allProducts = [
   {
-    label: "android"
-  }
-  ,{
-    label: "mobile"
-  }
-  ,{
-    label: "iphone"
-  }
-  ,{
-    label: "headphone"
-  }
-  ,{
-    label: "movies"
+    title: 'laptop & tablet',
+    image: '/assets/imgs/pro1.jpg',
+    desc: 'apple macBook pro with otuch bar mF841DSN/A 13',
+    price: 1.259,
+  },
+  {
+    title: 'headphones',
+    image: '/assets/imgs/pro2.jpg',
+    desc: 'Ultra Wireless S50 Headphones with Bluetooth',
+    price: 349,
+  },
+  {
+    title: 'Phones',
+    image: '/assets/imgs/pro3.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 449,
+  },
+  {
+    title: 'Gadgets',
+    image: '/assets/imgs/pro4.jpg',
+    desc: 'Apple Watch Sport 42mm New',
+    price: 349,
+  },
 
-  }
-  ,{
-    label: "computers"
+  {
+    title: 'laptop & tablet',
+    image: '/assets/imgs/pro1.jpg',
+    desc: 'MacBook Pro Retina Touch Bar 2.9GHz Intel Core i7 16GB',
+    price: 1.259,
+  },
+  {
+    title: 'Laptop & Tablet',
+    image: '/assets/imgs/pro5.jpg',
+    desc: 'MacBook Rose Gold 1.2GHz 256GB butterfly mechanism',
+    price: 1.299,
+  },
+  {
+    title: 'Phones',
+    image: '/assets/imgs/pro8.jpg',
+    desc: 'Apple iPhone 7 4.7-Inch Silver 128GB Contact-Free',
+    price: 250,
+  },
+  {
+    title: 'Phones',
+    image: '/assets/imgs/pro3.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 351,
+  },
+  {
+    title: 'gadgets, headphones',
+    image: '/assets/imgs/whitehead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 225,
+  },
+  {
+    title: 'android',
+    image: '/assets/imgs/and1.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 449,
+  },
+];
+export const laptops = [
+  {
+    title: 'laptop & tablet',
+    image: '/assets/imgs/pro1.jpg',
+    desc: 'apple macBook pro with otuch bar mF841DSN/A 13',
+    price: 1.259,
+  },
+  {
+    title: 'laptop & tablet',
+    image: '/assets/imgs/pro1.jpg',
+    desc: 'MacBook Pro Retina Touch Bar 2.9GHz Intel Core i7 16GB',
+    price: 1.259,
+  },
+  {
+    title: 'Laptop & Tablet',
+    image: '/assets/imgs/pro5.jpg',
+    desc: 'MacBook Rose Gold 1.2GHz 256GB butterfly mechanism',
+    price: 1.299,
+  },
+  {
+    title: 'Laptop & Tablet',
+    image: '/assets/imgs/and1.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 449,
+  },
+  {
+    title: 'Laptop & Tablet',
+    image: '/assets/imgs/whitehead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 225,
+  },
+];
+export const computers = [
+  {
+    title: 'Computers, Office',
+    image: '/assets/imgs/mac.jpg',
+    desc: 'MApple 21.5″ iMac® Intel Core i5 8GB 1TB Silver',
+    price: 1259,
+  },
+  {
+    title: 'Cameras&Video, Computers',
+    image: '/assets/imgs/mac.jpg',
+    desc: 'Apple 21.5-inch iMac Retina 4K Display',
+    price: 5559,
+  },
+  {
+    title: 'android',
+    image: '/assets/imgs/and1.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 449,
+  },
+  {
+    title: 'gadgets, headphones',
+    image: '/assets/imgs/whitehead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 225,
+  },
+];
 
-  } ,{
-    label: "laptops"
+export const allHeadPhones = [
+  {
+    title: 'headphones',
+    image: '/assets/imgs/pro2.jpg',
+    desc: 'Ultra Wireless S50 Headphones with Bluetooth',
+    price: 3149,
+  },
+  {
+    title: 'Gadgets',
+    image: '/assets/imgs/pro4.jpg',
+    desc: 'Apple Watch Sport 42mm New',
+    price: 349,
+  },
+  {
+    title: 'gadgets, headphones',
+    image: '/assets/imgs/whitehead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 225,
+  },
+  {
+    title: 'gadgets, headphones',
+    image: '/assets/imgs/cyanhead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 2225,
+  },
+];
+export const phones = [
+  {
+    title: 'iPhones',
+    image: '/assets/imgs/and2.jpg',
+    desc: 'Apple iPhone 6 4.7-Inch Black 128GB',
+    price: 449,
+  },
+  {
+    title: 'iPhones',
+    image: '/assets/imgs/and3.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 552,
+  },
+  {
+    title: 'android',
+    image: '/assets/imgs/and4.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 1112,
+  },
+  {
+    title: 'iPhones',
+    image: '/assets/imgs/and5.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 259,
+  },
+];
 
-  },{
-    label: "office"
+export const released = [
+  { title: 'Hot New Releases' },
+  { title: 'Top Selling Products' },
+  { title: 'Featured Products' },
+];
 
-  },{
-    label: "screen"
+export const sellingPro = [
+  {
+    image: '/assets/imgs/pro5.jpg',
+    desc: 'Apple VIP Membership',
+    price: 259,
+    oldPrice: 299,
+  },
+  {
+    image: '/assets/imgs/pro8.jpg',
+    desc: '	Apple iPad 9.7 128GB WiFi',
+    price: 999,
+  },
+  {
+    image: '/assets/imgs/pro9.jpg',
+    desc: '	Apple iPad 9.7 128GB WiFi',
+    price: 584,
+  },
+  { image: '/assets/imgs/pro2.jpg', desc: 'Apple VIP Membership', price: 259 },
+  {
+    image: '/assets/imgs/pro4.jpg',
+    desc: '	Apple iPad 9.7 128GB WiFi',
+    price: 999,
+  },
+  {
+    image: '/assets/imgs/pro8.jpg',
+    desc: '	Apple iPad 9.7 128GB WiFi',
+    price: 584,
+  },
+  {
+    image: '/assets/imgs/and5.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 259,
+  },
+  {
+    image: '/assets/imgs/iron.jpg',
+    desc: '	Apple iPad 9.7 128GB WiFi',
+    price: 999,
+  },
+  {
+    image: '/assets/imgs/whitehead.jpg',
+    desc: 'Apple iPhone 6s -16/32/64/ 128GB Grey',
+    price: 225,
+  },
+];
 
-  },{
-    label: "products"
+// footer data
 
-  }
-    ]
+export const footerData = [
+  { title: 'about us' },
+  { title: 'our services' },
+  { title: 'contact us' },
+  { title: 'stores' },
+  { title: 'order tracking' },
+];
+export const customerService = [
+  { title: 'My account' },
+  { title: 'order tracking' },
+  { title: 'pricing plans' },
+  { title: 'terms & conditions' },
 
-    export const categoryPage=[{label:"all categories",
-      list:page2}]
+  { title: 'order tracking' },
+  { title: 'FQA' },
+];
+
+export const quickLinks = [
+  { title: 'smartphones' },
+  { title: 'headphones' },
+  { title: 'Laptop & Tablet' },
+  { title: 'Gadgets' },
+];
+
+export const page1 = [
+  {
+    label: 'about us',
+    link: '/about',
+  },
+  {
+    label: 'our team',
+    link: '/our team',
+  },
+  {
+    label: 'our services',
+  },
+  {
+    label: 'pricing',
+  },
+  {
+    label: 'contact',
+  },
+  {
+    label: 'error 404',
+  },
+];
+// pages of navbar
+export const allPages = [
+  { label: 'home' },
+  { label: 'pages', list: page1 },
+  { label: 'shop' },
+  { label: 'blog' },
+  { label: 'elements' },
+];
+
+// pages of categories of header
+export const page2 = [
+  {
+    label: 'android',
+  },
+  {
+    label: 'mobile',
+  },
+  {
+    label: 'iphone',
+  },
+  {
+    label: 'headphone',
+  },
+  {
+    label: 'movies',
+  },
+  {
+    label: 'computers',
+  },
+  {
+    label: 'laptops',
+  },
+  {
+    label: 'office',
+  },
+  {
+    label: 'screen',
+  },
+  {
+    label: 'products',
+  },
+];
+
+export const categoryPage = [{ label: 'all categories', list: page2 }];
